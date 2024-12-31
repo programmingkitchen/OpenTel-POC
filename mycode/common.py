c64d2d79ed953c22aaab26af3a99853049132ec1
Randall Granier <programmingkitchen@gmail.com>
2024-12-31 13:36:48 -0600

    Troubleshooting BatchLogProcessor

--- mycode/common.py
+++ mycode/common.py
@@ -8,7 +8,7 @@ from opentelemetry.semconv.trace import SpanAttributes
 from local_machine_resource_detector import LocalMachineResourceDetector
 import logging
 # from opentelemetry.sdk._logs.export import ConsoleLogExporter, BatchLogProcessor
-from opentelemetry.sdk._logs.export import ConsoleLogExporter, SimpleLogProcessor
+from opentelemetry.sdk._logs.export import ConsoleLogExporter
 from opentelemetry.sdk._logs import (
     LogEmitterProvider,
     OTLPHandler,
