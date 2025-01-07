5201c3ac288ef1e5156c3e351e0b1366b79730a7
Randall Granier <programmingkitchen@gmail.com>
2025-01-07 15:31:28 -0500

    Add exporter code

---
+++ mycode/metrics-02.py
@@ -0,0 +1,19 @@
+from opentelemetry.metrics import set_meter_provider
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk._metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
+
+def configure_meter_provider():
+    exporter = ConsoleMetricExporter()
+    reader = PeriodicExportingMetricReader(exporter, export_interval_millis=5000)
+    provider = MeterProvider(
+        metric_readers=[reader],
+        resource=Resource.create())
+    set_meter_provider(provider)
+
+if __name__ == "__main__":
+    configure_meter_provider()
+    
